2be164f58679aaabe1aa4e387be93d0464a0b601
mdsamin <aminmdsadequl@gmail.com>
2021-03-30 17:28:26 +0600

    duplicate course can not be added!

--- src/extracurricularmanagement/FillInformationToAddYourCourse.java
+++ src/extracurricularmanagement/FillInformationToAddYourCourse.java
@@ -347,15 +347,23 @@ public class FillInformationToAddYourCourse extends javax.swing.JFrame {
         String classDay = classDay_jComboBox3.getSelectedItem().toString();;
         String classTime = classTime_jComboBox4.getSelectedItem().toString();
         String vacancies = vacancies_jComboBox5.getSelectedItem().toString();
-
+        
         Course course = new Course(coachID, courseID, expertise, classLocation, classDay, classTime, vacancies);
-        Data.courseList.add(course);
-
-        SuccessFullCourseAdd successFullCourseAdd = new SuccessFullCourseAdd();
-        successFullCourseAdd.setCourse(course);
-        successFullCourseAdd.setVisible(true);
-
-        this.dispose();
+        
+        Optional<Course> courseOptional = Data.courseList.stream()
+                .filter(c -> c.getCoachID().equals(Data.currentLoggedCoach) && c.getClassDay().equals(classDay) && c.getClassTime().equals(classTime))
+                .findFirst();
+        if (courseOptional.isPresent()) {
+            JOptionPane.showMessageDialog(rootPane, "Conflict Course!");
+        } else {
+            Data.courseList.add(course);
+            Data.courseList.forEach(c -> System.out.println(c.toString()));
+            SuccessFullCourseAdd successFullCourseAdd = new SuccessFullCourseAdd();
+            successFullCourseAdd.setCourse(course);
+            successFullCourseAdd.setVisible(true);
+            
+            this.dispose();
+        }
     }//GEN-LAST:event_save_buttonActionPerformed
 
     /**
