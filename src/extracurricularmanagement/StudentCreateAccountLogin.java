c104d64a6689e57604670acc94ed4c8778d5bcf5
mdsamin <aminmdsadequl@gmail.com>
2021-04-19 19:22:51 +0600

    bug fix : student ID generation !

--- src/extracurricularmanagement/StudentCreateAccountLogin.java
+++ src/extracurricularmanagement/StudentCreateAccountLogin.java
@@ -375,7 +375,7 @@ public class StudentCreateAccountLogin extends javax.swing.JFrame {
         } else {
             try {
                 phoneNumber = Integer.parseInt(phone);
-                Student student = new Student(new Random().nextInt(1000) + 1, name, phone, address);
+                Student student = new Student(Data.studentsList.size() + 1, name, phone, address);
                 Data.studentsList.add(student);
                 JOptionPane.showMessageDialog(rootPane, "Account Created Successfully!"
                         + "\n ID : " + student.getId());
