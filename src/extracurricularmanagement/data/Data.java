2dc9709301786a642013c818ad1cb82d7d1b77fd
mdsamin <aminmdsadequl@gmail.com>
2021-04-19 17:27:54 +0600

    student static add!

--- src/extracurricularmanagement/data/Data.java
+++ src/extracurricularmanagement/data/Data.java
@@ -21,15 +21,16 @@ import extracurricularmanagement.model.EnrolledCourses;
 public class Data {
 
     public static int courseCount = 1;
+    public static int studentCount = 1;
     public static String currentLoggedCoach = "";
     public static String currentLoggedStudent = "";
 
     public static List<Coach> coachListObject = Arrays.asList(new Coach[]{
-        new Coach("Klopp Kevin", "Klopp", "South beach road, Californina, CA 1215", "012345678"),
-        new Coach("Jose Anderson", "Jose", "15 Down Town, Los Angalose, LA 125", "02123456"),
-        new Coach("Pep Maven", "Pep", "15 Town Road, Virgenia, V 125", "03123456"),
-        new Coach("Brendan Torrent", "Brendan", "15 Town Hall, Alaska, Alaska 25", "05123456"),
-        new Coach("Ole Sabbir", "Ole", "15 Town Hall, Alaska, Alaska 25", "05789456")
+        new Coach("Klopp Kevin", "klopp", "South beach road, Californina, CA 1215", "012345678"),
+        new Coach("Jose Anderson", "jose", "15 Down Town, Los Angalose, LA 125", "02123456"),
+        new Coach("Pep Maven", "pep", "15 Town Road, Virgenia, V 125", "03123456"),
+        new Coach("Brendan Torrent", "brendan", "15 Town Hall, Alaska, Alaska 25", "05123456"),
+        new Coach("Ole Sabbir", "ole", "15 Town Hall, Alaska, Alaska 25", "05789456")
 //        0. "Klopp", 1. "Jose", 2."Pep", 3."Brendan", 4."Ole"
     });
 
@@ -49,8 +50,16 @@ public class Data {
         "10:00-11:00", "11:00-12:00", "12:00-13:00", "14:00-15:00", "15:00-16:00", "16:00-17:00", "10:00-11:00"
     });
     public static List<String> location = Arrays.asList(new String[]{
-        "Swiming Pool 1", "Swiming Pool 2", "Art School Yeard", "Art SChool Hall", "Art School Hall 2", "Math school Yeard", "Gym 1", "Gym 2"
+        "Swiming Pool 1",
+        "Swiming Pool 2",
+        "Art School Yeard",
+        "Art SChool Hall",
+        "Art School Hall 2",
+        "Math school Yeard",
+        "Gym 1",
+        "Gym 2"
     });
+
     public static List<Course> courseList = new ArrayList<>(Arrays.asList(
             new Course(Data.coachList.get(0),
                     Integer.toString(courseCount++),
@@ -87,8 +96,21 @@ public class Data {
     public static List<EnrolledCourses> enrolledCourses = new ArrayList<>();
     public static List<EnrolledCourses> cancelledCourses = new ArrayList<>();
     public static List<Student> studentsList = new ArrayList<>(Arrays.asList(
-            new Student(1, "A", "015", "XX"),
-            new Student(2, "B", "016", "YY")
+            new Student(101, "James", "61 Prince Drive. Macomb, MI 48042", "1-234-567-8911"),
+            new Student(102, "John", "50 Acacia Street. Elk Grove Village, IL 60007", "1-234-567-8912"),
+            new Student(103, "Robert", "920 West Helen Drive. Summerfield, FL 34491", "1-234-567-8913"),
+            new Student(104, "Christopher", "276 W. Lake Forest St. Burbank, IL 60459", "1-234-567-8914"),
+            new Student(105, "Daniel", "341 Vermont St. Henderson, KY 42420", "1-234-567-8915"),
+            new Student(106, "Paul", "544 James Rd. Grand Haven, MI 49417", "1-234-567-8916"),
+            new Student(107, "Ronald", "20 Prince Drive. Macomb, MI 48042", "1-234-567-8917"),
+            new Student(108, "Anthony", "13 Acacia Street. Elk Grove Village, IL 60007", "1-234-567-8918"),
+            new Student(109, "Kevin", "92 West Helen Drive. Summerfield, FL 34491", "1-234-567-8919"),
+            new Student(110, "Mary", "26 W. Lake Forest St. Burbank, IL 60459", "1-234-567-8920"),
+            new Student(111, "Patricia", "34 Vermont St. Henderson, KY 42420", "1-234-567-8921"),
+            new Student(112, "Lisa", "54 James Rd. Grand Haven, MI 49417", "1-234-567-8922"),
+            new Student(113, "Nancy", "6 Prince Drive. Macomb, MI 48042", "1-234-567-8923"),
+            new Student(114, "Michelle", "5 Acacia Street. Elk Grove Village, IL 60007", "1-234-567-8924"),
+            new Student(115, "Laura", "92 West Helen Drive. Summerfield, FL 34491", "1-234-567-8925")
     ));
 
     public static int getVacancy() {
